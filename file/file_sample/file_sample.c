ee3aa25fb9bdef5d07a0df2566817255c2d11f2d
Toan <ngosytoan5797dlna@gmail.com>
2021-06-01 20:58:39 +0700

    Add code in /file/file_sample for print value of fd of a process

--- file/file_sample/file_sample.c
+++ file/file_sample/file_sample.c
@@ -9,8 +9,11 @@
 void main(void)
 {
 	int fd;
+	int fd1;
 
-	fd = open("mytext", O_CREAT | O_WRONLY);
+	fd = open("mytext1", O_CREAT | O_WRONLY);
+	fd1 = open("mytext2", O_CREAT | O_WRONLY);
+	printf("fd = %d, fd1 = %d", fd, fd1);
 	write(fd, "Hello world\n", strlen("Hello world\n"));
 
 	close(fd);
